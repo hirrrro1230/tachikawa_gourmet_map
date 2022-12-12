7a66c1e4d0eab3a2a4ba7bf651bc647a878c489d
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-12-12 13:44:35 +0000

    [Favorites]edit12/12

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,17 @@
 class Public::FavoritesController < ApplicationController
     def create
-        restaurant = Restaurant.find(params[:restaurant_id])
-        favorite = current_customer.favorites.new(restaurant_id: restaurant.id)
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        favorite = current_customer.favorites.new(restaurant_id: @restaurant.id)
         favorite.save
         #非同期処理を実装
-        redirect_to restaurant_path(restaurant), notice: "いいねしました！"
+        #redirect_to restaurant_path(restaurant), notice: "いいねしました！"
     end
 
     def destroy
-        restaurant = Restaurant.find(params[:restaurant_id])
-        favorite = current_customer.favorites.find_by(restaurant_id: restaurant.id)
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        favorite = current_customer.favorites.find_by(restaurant_id: @restaurant.id)
         favorite.destroy
         #非同期処理を実装
-        redirect_to restaurant_path(restaurant), alert: "いいねを取り消しました"
+        #redirect_to restaurant_path(restaurant), alert: "いいねを取り消しました"
     end
 end
